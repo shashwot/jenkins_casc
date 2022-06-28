64e5ae057ce38b36d48a8e68b8289a1a8b0e2af2
shashwot <shashwot@gmail.com>
2022-06-28 13:36:05 +0545

    jenkinsfile changes

--- jobdsl/SNYK/snyk.groovy
+++ jobdsl/SNYK/snyk.groovy
@@ -1,4 +1,5 @@
 job('SECURITY-SAST-SNYK') {
+  withCredentials([string(credentialsId: 'snyk_auth', variable: 'snyk_auth')]) {
   steps {
     shell('''cd ${WORKSPACE}
 SNYK_TOKEN="${snyk_auth}" snyk test 
