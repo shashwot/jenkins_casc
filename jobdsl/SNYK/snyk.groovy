17850d62f69d791f842133b4bafcb43710e00914
shashwot <shashwot@gmail.com>
2022-06-28 13:48:36 +0545

    jenkinsfile changes

--- jobdsl/SNYK/snyk.groovy
+++ jobdsl/SNYK/snyk.groovy
@@ -3,6 +3,33 @@ job('SECURITY-SAST-SNYK') {
         credentialsBinding {
             string('snyk_auth', 'snyk_auth')
         }
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+        publishers {
+        cleanWs { // Clean after build
+            cleanWhenAborted(true)
+            cleanWhenFailure(true)
+            cleanWhenNotBuilt(false)
+            cleanWhenSuccess(true)
+            cleanWhenUnstable(true)
+            deleteDirs(true)
+            notFailBuild(true)
+            disableDeferredWipeout(true)
+            patterns {
+                pattern {
+                    type('EXCLUDE')
+                    pattern('.propsfile')
+                }
+                pattern {
+                    type('INCLUDE')
+                    pattern('.gitignore')
+                }
+            }
+        }
+        }
   steps {
     shell('''cd ${WORKSPACE}
 SNYK_TOKEN="${snyk_auth}" snyk test 
